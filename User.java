83e27ed58edf65ebca0db9114be72b786b823c9b
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-01-31 15:48:34 +0530

    Add files via upload

--- User.java
+++ User.java
@@ -3,8 +3,18 @@ public class User {
     public static void main(String args[])
 {
     Scanner sc=new Scanner(System.in);
-     System.out.println("enter the age");
+    System.out.println("enter the age");
     int age=sc.nextInt();
+    float f=sc.nextFloat();
+    double d=sc.nextDouble();
+    String s1=sc.nextLine();
+    String s2=sc.next();
+    
     System.out.println(age);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(s1);
+    System.out.println(s2);
+
 }    
 }
