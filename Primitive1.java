7963fba3355ce44ba0429e9a934767a89704c8da
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-02-07 10:54:54 +0530

    Add files via upload

---
+++ Primitive1.java
@@ -0,0 +1,15 @@
+public class Primitive1 {
+    public static void main(String[] args) {
+        String name="Nasreen";
+        int age=19;
+         char grade='A';
+         int marks=99;
+         String status="Pass";
+         System.out.println(name);
+         System.out.println(age);
+         System.out.println(grade);
+         System.out.println(marks);
+          System.out.println(status);
+    }
+    
+}
