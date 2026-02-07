b99ceaf8b1bcf945da55e233a6b715d207c653fc
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-02-07 10:55:18 +0530

    Add files via upload

---
+++ Primitive2.java
@@ -0,0 +1,12 @@
+public class Primitive2 {
+    public static void main(String[] args) {
+        int s1=90;
+        int s2=80;
+        int s3=70;
+        int total=s1+s2+s3;
+        float average=total/3;
+        System.out.println(total);
+        System.out.println(average);
+    }
+    
+}
