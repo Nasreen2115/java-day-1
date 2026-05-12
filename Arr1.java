a9ecd6b987035fcbda5078d8cfb8f60c0f49996e
Nasreen2115 <nasreen2115shaik@gmail.com>
2026-05-12 11:34:24 +0530

    changes done

--- Arr1.java
+++ Arr1.java
@@ -1,6 +1,6 @@
 public class Arr1 {
     public static void main(String[] args) {
-        int arr[]={10,20,30,40};
+        int arr[]={1,20,30,40};
         int sum=0;
          for(int i=0;i<arr.length;i++)
          {
